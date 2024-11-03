7c3baf1d1481fd96d0ce4d22b211af923a7546cc
sandy <sandysukma70@gmail.com>
2024-11-03 20:30:38 +0700

    Sebelum turu wkwk

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,6 +3,23 @@ const Category = require('../models/categoryModel');
 const getCategories = (req, res) => Category.getAll((err, results) => err ? res.status(500).send(err) : res.json(results));
 const getCategoryById = (req, res) => Category.getById(req.params.id, (err, results) => results.length === 0 ? res.status(404).send('Category not found') : res.json(results[0]));
 const createCategory = (req, res) => Category.create(req.body, (err, results) => res.status(201).json({ id: results.insertId, ...req.body }));
-const updateCategory = (req, res) => Category.update(req.params.id, req.body, (err, results) => results.affectedRows === 0 ? res.status(404).send('Category not found') : res.json({ id: req.params.id, ...req.body }));
+// const updateCategory = (req, res) => Category.update(req.params.id, req.body, (err, results) => results.affectedRows === 0 ? res.status(404).send('Category not found') : res.json({ id: req.params.id, ...req.body }));
+const updateCategory = (req, res) => {
+    console.log('Updating category with ID:', req.params.id);
+    console.log('Data received:', req.body);
+    
+    Category.update(req.params.id, req.body, (err, results) => {
+      if (err) {
+        console.error('Update error:', err); // Log error jika ada
+        return res.status(500).send('Internal Server Error');
+      }
+      if (results.affectedRows === 0) {
+        return res.status(404).send('Category not found');
+      }
+      res.json({ id: req.params.id, ...req.body });
+    });
+  };
+  
+  
 const deleteCategory = (req, res) => Category.delete(req.params.id, (err, results) => results.affectedRows === 0 ? res.status(404).send('Category not found') : res.json({ message: 'Category deleted' }));
 module.exports = { getCategories, getCategoryById, createCategory, updateCategory, deleteCategory };
--- models/categoryModel.js
+++ models/categoryModel.js
@@ -3,7 +3,12 @@ const Category = {
   getAll: (callback) => db.query('SELECT * FROM categories', callback),
   getById: (id, callback) => db.query('SELECT * FROM categories WHERE id = ?', [id], callback),
   create: (data, callback) => db.query('INSERT INTO categories SET ?', data, callback),
-  update: (id, data, callback) => db.query('UPDATE categories SET ? WHERE id = ?', [data, id], callback),
+  update: (id, data, callback) => 
+    db.query(
+      'UPDATE categories SET name = ?, description = ? WHERE id = ?', 
+      [data.name, data.description, id],
+      callback
+    ),
   delete: (id, callback) => db.query('DELETE FROM categories WHERE id = ?', [id], callback)
 };
 module.exports = Category;
